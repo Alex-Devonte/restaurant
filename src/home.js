87b497d4b1f0799db9f72f236c845eb2d6527380
Alex <alex95.taylor@gmail.com>
2023-11-12 16:51:31 -0500

    Add styles and layout and image for home page

--- src/home.js
+++ src/home.js
@@ -1,8 +1,32 @@
+// function createHome() {
+//     const home = document.createElement('h1');
+//     home.textContent = "Homepage";
+//     return home;
+// }
+import burger from './images/about-burger.jpg';
+
 function createHome() {
-    const home = document.createElement('h1');
-    home.textContent = "Homepage";
+    const home = document.createElement('div');
+    home.id = 'home-content';
+
+    const aboutHeading = document.createElement('h1');
+    aboutHeading.textContent = 'About';
+    home.appendChild(aboutHeading);
+
+    const aboutText = document.createElement('p');
+    aboutText.textContent = 'Fake Burger Spot is a fake restaurant created in 2023. We serve specialty fake burgers, fake fries, and fake shakes as well!';
+    home.appendChild(aboutText);
+
+    const burgerImage = new Image();
+    burgerImage.src = burger;
+    burgerImage.alt = 'About Burger';
+    burgerImage.classList.add('about-burger');
+    home.appendChild(burgerImage);
+  
+
     return home;
 }
+
 
 function displayHome() {
     const main = document.getElementById("main-content");
