66ca9c2869c2b83c8206ecd3346858e1360a13a6
Alex <alex95.taylor@gmail.com>
2023-11-13 18:20:57 -0500

    Add style and layout for contact page

--- src/contact.js
+++ src/contact.js
@@ -1,11 +1,28 @@
 function createContact() {
-    const contact = document.createElement('h1');
-    contact.textContent = "Contact Page";
+    const contact = document.createElement('div');
+    contact.id = 'contact-content';
+
+    const contactHeading = document.createElement('h1');
+    contactHeading.textContent = "Contact Us!";
+    contact.appendChild(contactHeading);
+
+    const contactPhone = document.createElement('p');
+    contactPhone.textContent = '555-555-5555';
+    contact.appendChild(contactPhone);
+
+    const contactEmail = document.createElement('p');
+    contactEmail.textContent = 'fakeburger@gmail.com';
+    contact.appendChild(contactEmail);
+
+    const contactAddress = document.createElement('p');
+    contactAddress.textContent = '124 Fake Street';
+    contact.appendChild(contactAddress);
+    
     return contact;
 }
 
 function displayContact() {
-    const main = document.getElementById("main-content");
+    const main = document.getElementById('main-content');
     main.textContent = '';
     main.appendChild(createContact());
 }
