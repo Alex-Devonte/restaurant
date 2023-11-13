2221511daf5026473d72bfa40880726becabcb95
Alex <alex95.taylor@gmail.com>
2023-11-13 18:54:17 -0500

    Add logic for setting active class on tab

--- src/index.js
+++ src/index.js
@@ -9,9 +9,40 @@ loadSite();
 
 //Hook the tabbed nav elements with event listeners for switching main content
 function setupNav() {
-    const home = document.querySelector('#home').addEventListener('click', displayHome);
-    const menu = document.querySelector('#menu').addEventListener('click', displayMenu);
-    const contact = document.querySelector('#contact').addEventListener('click', displayContact);
+    const home = document.querySelector('#home'); 
+    const menu = document.querySelector('#menu');
+    const contact = document.querySelector('#contact');
+
+    home.className = 'active';
+
+
+    const tabs = [home, menu, contact];
+
+    //Loop through tabs to properly set active class based on page
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            //Remove active class from the other tabs
+            tabs.forEach(tab => tab.classList.remove('active'));
+
+            tab.className = 'active';
+
+            switch (tab) {
+                case home:
+                    displayHome();
+                    break;
+
+                case menu:
+                    displayMenu();
+                    break;
+
+                case contact:
+                    displayContact();
+                    break;
+                default:
+                    break;
+            }
+        });
+    });
 }
 
 function loadSite() {
